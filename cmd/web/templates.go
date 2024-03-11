695dc1378ed63be07b3a005ac5828c95520aa65d
Nika <freexnick@yahoo.com>
2024-03-11 23:35:01 +0400

    chapter13:embed

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"io/fs"
 	"lets-go-snippetbox/internal/models"
+	"lets-go-snippetbox/ui"
 	"path/filepath"
 	"text/template"
 	"time"
@@ -28,7 +30,7 @@ var functions = template.FuncMap{
 func newTemplateCache() (map[string]*template.Template, error) {
 	cache := map[string]*template.Template{}
 
-	pages, err := filepath.Glob("./ui/html/pages/*.tmpl")
+	pages, err := fs.Glob(ui.Files, "./ui/html/pages/*.tmpl")
 	if err != nil {
 		return nil, err
 	}
@@ -36,7 +38,13 @@ func newTemplateCache() (map[string]*template.Template, error) {
 	for _, page := range pages {
 		name := filepath.Base(page)
 
-		ts, err := template.New(name).Funcs(functions).ParseFiles("./ui/html/base.tmpl")
+		patterns := []string{
+			"html/base.tmpl",
+			"html/partials/*.tmpl",
+			page,
+		}
+
+		ts, err := template.New(name).Funcs(functions).ParseFS(ui.Files, patterns...)
 		if err != nil {
 			return nil, err
 		}
